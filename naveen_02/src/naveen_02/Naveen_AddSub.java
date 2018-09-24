cd4c2ac6fff1cd907ae57dc6b758b62767988fec
lavanya9100 <lavanyanaveen55@gmail.com>
2018-09-24 22:40:45 +0530

    "naveen_2nd commit Add_Sub"

---
+++ naveen_02/src/naveen_02/Naveen_AddSub.java
@@ -0,0 +1,15 @@
+package naveen_02;
+
+public class Naveen_AddSub {
+
+	public static void main(String[] args) {
+		int a=10,b=55;
+		int Add=a+b;
+		int sub=a-b;
+		System.out.println("Add value is :" +Add);
+		System.out.println("sub value is :" +sub);
+		
+		
+	}
+
+}
